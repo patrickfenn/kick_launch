69af302e754004c899f4031ee3b3680a86bedee6
pfenn <patrickdavidfenn@gmail.com>
2024-09-04 01:10:15 -0700

    Clean up code so it is easier to follow

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ Usage:
 [python3] kick_launch.py {channel name}
 
 Reqs:
-1.  Streamlink is installed.
-1a. Streamlink is in default path or STREAMLINK_PATH defined in script.
-2.  BROWSER_PATH must be defined in script.
+1. Streamlink is installed.
+2. Streamlink is in default path or STREAMLINK_PATH defined in script.
+3. BROWSER_PATH must be defined in script.
 
 Notes:
 1. Tested only on mac.
@@ -15,6 +15,11 @@ Notes:
 3. I did try to have a more fully functional program which fetched details from api given profile id.
     But kick is scuffed atm and doesn't have api public + wraps everything in cloudflare. So this is
     good enough for now.
+4. For ease of use, I recommend appending .zshrc (or another shell's config) with function:
+    kick () {
+        {path of kick_chat.py} $1
+    }
+    Then can just use: `kick {channel}` in shell. Remember to source .zshrc or restart terminal.
 
 Errors:
-403: Happens when cloudflare gets too many requests in a period of time. Will need to wait ~10 minutes.
+403: See https://github.com/streamlink/streamlink/issues/6018 . I typically can wait some time (~10 min) and it will work again.
--- kick_launch.py
+++ kick_launch.py
@@ -4,20 +4,7 @@
 Usage:
 [python3] kick_launch.py {channel name}
 
-Reqs:
-1.  Streamlink is installed.
-1a. Streamlink is in default path or STREAMLINK_PATH defined below.
-2.  BROWSER_PATH must be defined below.
-
-Notes:
-1. Tested only on mac.
-2. Recommend disabling auto resize in vlc / whatever player streamlink uses.
-3. I did try to have a more fully functional program which fetched details from api given profile id.
-    But kick is scuffed atm and doesn't have api public + wraps everything in cloudflare. So this is
-    good enough for now.
-
-Errors:
-403: Happens when cloudflare gets too many requests in a period of time. Will need to wait ~10 minutes.
+Will need to change shebang if python3 path is different and don't want to prefix python3.
 """
 
 import os
@@ -29,15 +16,34 @@ import sys
 BROWSER_PATH = "/Applications/Firefox.app"
 STREAMLINK_PATH = None
 
-def main():
-    streamlink_path = ""
+def args_are_invalid():
+    if (sys.argv[0].find("python") != -1):
+        if (len(sys.argv) != 3):
+            print("Expected 3 args")
+            return 1
+        else:
+            if (len(sys.argv) != 2):
+                print("Expected 2 args")
+                return 1
+    return 0
+
+def get_streamlink_path():
     if not STREAMLINK_PATH:
         streamlink_path = shutil.which("streamlink")
         if not streamlink_path:
             print("Could not find streamlink path in PATH")
-            return 1
+            return None
     else:
         streamlink_path = STREAMLINK_PATH
+    return streamlink_path
+
+def main():
+    if args_are_invalid():
+        return 1
+
+    streamlink_path = get_streamlink_path()
+    if not streamlink_path:
+        return 2
 
     channel_name = sys.argv[-1]
     base_url = f"https://www.kick.com/{channel_name}"
@@ -46,12 +52,13 @@ def main():
     if streamlink_pid == 0:
         args = [streamlink_path,
                 "-a", "--play-and-exit",
+                "--http-header=Accept-Language=en-US,en;q=0.9",
                 base_url, "best"]
         env = os.environ.copy()
         os.execve(streamlink_path, args, env)
     elif streamlink_pid < 0:
         print ("Issue with forking streamlink")
-        return 2
+        return 3
     else:
         chat_url = f"{base_url}/chatroom"
         browser = webbrowser.get(f"open -a {BROWSER_PATH} %s")
